e93de7a5d43c5628c0e4d3b11f6411ec0122934e
Akihiro Nagashima <ei.ei.oh.0613@gmail.com>
2016-08-10 14:39:09 +0900

    デザイン調整

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -1,77 +1,110 @@
 $(function() {
   var _syncMap;
-  var _identity;
+  var _consultant
+
+  class Consultant {
+    constructor(identity, topic) {
+      this.identity = identity;
+      this.topic = topic;
+      this.status = "";
+      $.trigger("initConsultant", this);
+    }
+
+    changeStatus(status) {
+      this.status = status
+      _syncMap.set(this.identity, {status: this.status, topic: this.topic});
+      $.trigger("changedConsultantStatus", this);
+    }
+
+    goToOffline() {
+      _syncMap.remove(this.identity);
+    }
+  }
+
+  $.one("initConsultant", function (e, consultant) {
+    $("p#UserInfo").text(consultant.identity+": "+consultant.topic);
+    $("p#UserInfo").show();
+    $("#CallOperations").show();
+  });
+
+  $.on("changedConsultantStatus", function (e, consultant) {
+    $("p#UserStatus").text(consultant.status);
+    $("p#UserStatus").show();
+  });
 
   var setUpSync = function(token) {
     accessManager = new Twilio.AccessManager(token);
     syncClient = new Twilio.Sync.Client(accessManager);
     syncClient.map("consultation.members").then(function(map) {
       _syncMap = map;
-      _syncMap.set(_identity, {status: "Ready"});
-      updateMembersView();
-
-      _syncMap.on('itemUpdated', function() {
-        updateMembersView();
-      });
-
-      _syncMap.on('itemAdded', function() {
-        updateMembersView();
-      });
-
-      _syncMap.on('itemRemoved', function() {
-        updateMembersView();
+      _consultant.changeStatus("待受中");
+      updateOnlineConsultantsView();
+      $.each(["itemUpdated", "itemAdded", "itemRemoved"], function(index, action) {
+        _syncMap.on(action, function() {
+          updateOnlineConsultantsView();
+        });
       });
     });
   };
 
-  var updateMembersView = function() {
-    _syncMap.getItems().then(function(page) {
-      $("#Members").html("");
-      $.each(page.items, function( index, item ) {
-        if (item._key != _identity) {
-          $("#Members").append('<li><a href="#" data-indentity="' + item._key + '">' + item._key + '（' + item._value.status + '）</a></li>');
-        }
-      });
-    });
-  }
-
   var setUpPhone = function(token) {
-    // Twilio.Device.setup(token, {debug: true});
     Twilio.Device.setup(token);
 
     Twilio.Device.ready(function (device) {
-      $("#CallInfo p.log").text("Ready");
+      _consultant.changeStatus("待受中");
     });
 
     Twilio.Device.connect(function (conn) {
-      _syncMap.set(_identity, {status: "Calling"});
-      $("#CallInfo p.log").text("Calling");
+      _consultant.changeStatus("通話中");
     });
 
     Twilio.Device.cancel(function (conn) {
-      _syncMap.set(_identity, {status: "Ready"});
-      $("#CallInfo p.log").text("Ready");
+      _consultant.changeStatus("待受中");
     });
 
     Twilio.Device.disconnect(function (conn) {
-      _syncMap.set(_identity, {status: "Ready"});
-      $("#CallInfo p.log").text("Ready");
+      _consultant.changeStatus("待受中");
     });
 
     Twilio.Device.incoming(function (conn) {
-      _syncMap.set(_identity, {status: "Calling"});
-      $("#CallInfo p.log").text("Incoming");
+      _consultant.changeStatus("入電中");
     });
 
     Twilio.Device.offline(function (device) {
-      _syncMap.remove(_identity);
-      $("#CallInfo p.log").text("Offline");
+      _consultant.goToOffline();
+    });
+
+    $("#Accept").click(function() {
+      Twilio.Device.activeConnection().accept();
+    });
+
+    $("#Disconnect").click(function() {
+      Twilio.Device.disconnectAll();
+    });
+
+    $("#OnlineConsultants").on("click", "a", function() {
+      params = {
+        "from_client_id": _consultant.identity,
+        "to_client_id": $(this).data("indentity")
+      };
+      Twilio.Device.connect(params);
+      return false
     });
   };
 
-  var initWithIdentity = function(identity) {
-    _identity = identity;
-    $("#CallInfo p.name").text("you are ["+ _identity +"]")
+  var updateOnlineConsultantsView = function() {
+    _syncMap.getItems().then(function(page) {
+      $("#OnlineConsultants").html("");
+      $.each(page.items, function(index, item) {
+        if (item._key != _consultant.identity) {
+          $("#OnlineConsultants").append('<li><a href="#" data-indentity="' + item._key + '">' + item._key + ': ' + item._value.topic + ' / ' + item._value.status + '</a></li>');
+        }
+      });
+    });
+  };
+
+  var goToOnline = function(identity, topic) {
+    _consultant = new Consultant(identity, topic)
     var gettingSyncToken = $.getJSON("/sync_token", {identity: identity});
     var gettingPhoneToken = $.getJSON("/phone_token", {identity: identity});
     $.when(gettingSyncToken, gettingPhoneToken).done(function (sync, phone) {
@@ -80,35 +113,17 @@ $(function() {
     });
   };
 
-  $("#Accept").click(function() {
-    Twilio.Device.activeConnection().accept();
-  });
-
-  $("#Disconnect").click(function() {
-    Twilio.Device.disconnectAll();
-  });
-
   $("#GoToOnline").click(function() {
     if ($("#Identity").val() != "") {
-      initWithIdentity($("#Identity").val());
+      goToOnline($("#Identity").val(), $("#Topic").val());
       $("#IdentityForm").hide();
     }
     return false
   });
 
-  $("#Members").on("click", "a", function() {
-    params = {
-      "from_client_id": _identity,
-      "to_client_id": $(this).data("indentity")
-    };
-    Twilio.Device.connect(params);
-    return false
-  });
-
   $(window).unload(function() {
-    if(_identity) {
-      console.log(_identity);
-      _syncMap.remove(_identity);
+    if(_consultant) {
+      _syncMap.remove(_consultant.identity);
     }
   });
 });
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'index#index'
   get 'sync_token', to: "tokens#sync"
   get 'phone_token', to: "tokens#phone"
-  post 'voice', to: "consultation#voice"
+  post 'voice', to: "index#voice"
 end
